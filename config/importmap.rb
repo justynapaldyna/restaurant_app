cd3e92cd5fe8ae0644ba1bbdfe944645b440f698
justynapaldyna <justyna.paldyna@gmail.com>
2023-03-14 18:28:23 +0100

    shopping cart and orders

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,9 @@
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "jquery", to: "jquery.min.js", preload: true
+pin "jquery_ujs", to: "jquery_ujs.js", preload: true
+pin "jquery-ui", to: "jquery-ui.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
